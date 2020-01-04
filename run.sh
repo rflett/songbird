e6c00f26230a092bb6b856cd5734d74b61c506fc
root <root@jay-the-triple.australia-southeast1-b.c.triplej-countdown.internal>
2020-01-04 12:50:39 +0000

    Update scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker rm songbird
-docker run -d --name songbird --net=host -e API_URL=$API_URL -e CONSUMER_KEY=$CONSUMER_KEY -e CONSUMER_SECRET=$CONSUMER_SECRET -e ACCESS_TOKEN=$ACCESS_TOKEN -e ACCESS_SECRET=$ACCESS_SECRET songbird
+docker run -d --rm --name songbird --net=host -e API_URL=$API_URL -e CONSUMER_KEY=$CONSUMER_KEY -e CONSUMER_SECRET=$CONSUMER_SECRET -e ACCESS_TOKEN=$ACCESS_TOKEN -e ACCESS_SECRET=$ACCESS_SECRET songbird
 echo "Tailing logs.."
 docker logs -f songbird
